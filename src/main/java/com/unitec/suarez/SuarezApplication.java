9bd0cf57c30e5ef7185eb75563ecf22766a0de33
crsc_ <crsc_@LAPTOP-1S0SPLDC>
2020-07-03 19:40:05 -0500

    Primer Commit a GitHub

---
+++ src/main/java/com/unitec/suarez/SuarezApplication.java
@@ -0,0 +1,13 @@
+package com.unitec.suarez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuarezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuarezApplication.class, args);
+	}
+
+}
